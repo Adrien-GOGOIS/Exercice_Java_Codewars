8097403b3971e66351006aae86eec195894371ed
Adrien GOGOIS <ad.gogois@gmail.com>
2022-03-29 17:17:08 +0200

    Exo convert Integer into String

--- src/App.java
+++ src/App.java
@@ -1,7 +1,11 @@
 import codewars.*;
 
+// Convert number in parameter into String :
+
 public class App {
     public static void main(String[] args) {
+        NumberToString test = new NumberToString();
+        System.out.println(test.numberToString(45));
 
     }
 }
---
+++ src/codewars/NumberToString.java
@@ -0,0 +1,10 @@
+package codewars;
+
+public class NumberToString {
+    public static String numberToString(int num) {
+        String str = Integer.toString(num);
+        return str;
+    }
+
+    // Ou : return String.valueOf(num);
+}
